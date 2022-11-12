63b2d576c9ffe16adf57562bc5b1ffd6709f2829
feeleep <aliup45@gmail.com>
2022-11-12 16:58:12 +0100

    commiting

--- app.js
+++ app.js
@@ -1,7 +1,40 @@
 const express = require("express");
+const csvTojson = require("csvtojson");
+const fs = require("fs");
+const server = require("./server");
+const path = require("path");
+const cors = require("cors");
+const helmet = require("helmet");
 const app = express();
 
-const PORT = 8080;
-app.listen(PORT, () => {
-  console.log("Server started at port 8080");
+//Check if the file is csv
+
+app.use(cors());
+app.use(helmet());
+app.use(json());
+app.use(
+  urlencoded({
+    extended: true,
+  })
+);
+
+app.get("/", (req, res) => {
+  fs.readFile("./form.html", "utf-8", (err, data) => {
+    if (err) {
+      return res.send("error");
+    }
+    res.send(data);
+  });
 });
+app.post("/csv", csvUpload);
+
+fs.readFileSync(`${__dirname}/SampleCSV - Sheet1.csv`);
+csvTojson()
+  .fromFile("./SampleCSV - Sheet1.csv")
+  .then((source) => {
+    console.log(source);
+  });
+app.get("/", (req, res) => {
+  console.log("here");
+});
+module.exports = app;
---
+++ csv/fileCheck.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const multer = require("multer");
+const { v4: uuid } = require("uuid");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads");
+  },
+  filename: function (req, file, cb) {
+    cb(null, uuid() + ".csv");
+  },
+});
+
+// Define the maximum size for uploading
+const maxSize = 1 * 1000 * 1000;
+
+exports.upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: maxSize,
+  },
+  fileFilter: function (req, file, cb) {
+    const filetypes = /csv/;
+    const mimetype = filetypes.test(file.mimetype);
+
+    const extname = filetypes.test(
+      path.extname(file.originalname).toLowerCase()
+    );
+
+    if (mimetype && extname) {
+      return cb(null, true);
+    }
+
+    cb(
+      "Error: File upload only supports the " +
+        "following filetypes - " +
+        filetypes
+    );
+  },
+}).single("csv");
